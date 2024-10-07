d9f3a94a05fb746869a2580fa1351ad09dd51263
Lesly Estefanía Flores Rivera <147342430+lesflores@users.noreply.github.com>
2024-10-06 21:07:00 -0600

    Update DiD.R

--- DiD.R
+++ DiD.R
@@ -1,4 +1,3 @@
-# -------------------------------------------------------------------------------------
 # Diferencias en diferencias (DiD)
 # -------------------------------------------------------------------------------------
 # Ejemplo con la base de datos "evaluation.dta" del Banco Mundial (BM)
@@ -6,7 +5,6 @@
 # no inscrita (grupo de comparación). Este método es útil cuando las reglas de asignación del programa no son completamente claras, es decir, no hay asignación aleatoria.
 
 # Librerías
-
 library(fixest) # Para modelos DiD
 library(haven) # Para leer archivos .dta
 
@@ -19,7 +17,6 @@ datos <- datos[datos$treatment_locality == 1, ]
 # Crear la variable de interacción. La interacción combina dos variables: si el hogar está inscrito en el programa (enrolled) y si el periodo es antes o después de la intervención (round).
 datos$enrolled_round <- datos$enrolled * datos$round
 
-
 # Realizar la regresión DiD 
 did_modelo <- feols(health_expenditures ~ enrolled_round + round + enrolled, 
                    cluster = "locality_identifier", 
@@ -28,16 +25,15 @@ did_modelo <- feols(health_expenditures ~ enrolled_round + round + enrolled,
 # Resultados del modelo DiD
 summary(did_modelo)
 
-
 # Explicación de las variables utilizadas en el modelo:
-# 1. Gastos en salud (health_expenditures): Esta es la variable que queremos analizar. Lo que buscamos es entender cómo se modifican los gastos en salud de los hogares  debido a la intervención del programa.
-# 2. Inscripción e intervención (enrolled_round): Esta variable refleja el impacto directo del programa. Nos permite medir cuánto cambiaron los gastos en salud específicamente para los hogares que estuvieron inscritos 
+# 1. health_expenditures: Esta es la variable que queremos analizar. Lo que buscamos es entender cómo se modifican los gastos en salud de los hogares debido a la intervención del programa.
+# 2. enrolled_round: refleja el impacto directo del programa. Nos permite medir cuánto cambiaron los gastos en salud específicamente para los hogares que estuvieron inscritos 
 # en el programa después de la intervención.
-# 3. Tiempo (round): Esta variable controla los cambios generales que ocurrieron con el tiempo. Es decir, tiene en cuenta las variaciones que pueden haber afectado a todos los hogares, ya sean inscritos o no.
-# 4. Inscripción antes del tratamiento (enrolled): Aquí nos aseguramos de controlar por las diferencias iniciales entre los hogares que estaban inscritos y los que no, para observar si los hogares inscritos ya presentaban 
+# 3. round: controla los cambios generales que ocurrieron con el tiempo. Es decir, tiene en cuenta las variaciones que pueden haber afectado a todos los hogares, ya sean inscritos o no.
+# 4. enrolled: Aquí nos aseguramos de controlar por las diferencias iniciales entre los hogares que estaban inscritos y los que no, para observar si los hogares inscritos ya presentaban 
 # diferencias en sus gastos en salud antes de que el programa empezara.
-# 5. Agrupación por localidades (cluster = "locality_identifier"): Al agrupar los datos por localidad, ajustamos los errores estándar para tener en cuenta las similitudes que pueden existir entre hogares de una misma zona.
+# 5. cluster = "locality_identifier": Al agrupar los datos por localidad, ajustamos los errores estándar para tener en cuenta las similitudes que pueden existir entre hogares de una misma zona.
 
 # Qué significa la interacción entre inscripción y tiempo (enrolled * round)? Al combinar estas dos variables, creamos una nueva que nos indica cuándo un hogar fue impactado por el programa. 
-# Esta interacción solo tomará el valor de 1 si el hogar estaba inscrito y ya estamos en el periodo posterior a la intervención. Si alguna de estas dos condiciones no se cumple, es decir, si el hogar no estaba inscrito 
-# o si estamos en un momento previo al programa, el valor de la interacción será 0. Esto nos ayuda a aislar el efecto real del programa.
+# Esta interacción solo tomará el valor de 1 si el hogar estaba inscrito y ya estamos en el periodo posterior a la intervención. Si alguna de estas dos condiciones no se cumple, es decir, 
+# si el hogar no estaba inscrito o si estamos en un momento previo al programa, el valor de la interacción será 0. Esto nos ayuda a aislar el efecto real del programa.
